d80157371edaf5a89c94ce4a746e3cefcb149f0d
Ben Goltz <c_bgoltz@gmail.com>
2024-08-07 14:36:07 -0500

    changes environment reference

--- .infra/main.tf
+++ .infra/main.tf
@@ -59,14 +59,9 @@ data "github_repository" "repo" {
   full_name = "beta754/circles-the-game"
 }
 
-resource "github_repository_environment" "primaryk8s" {
-  repository  = data.github_repository.repo.name
-  environment = "primaryk8s"
-}
-
 resource "github_actions_environment_secret" "kube_config" {
   repository      = data.github_repository.repo.name
-  environment     = github_repository_environment.primaryk8s.environment
+  environment     = "primary"
 
   secret_name     = "KUBE_CONFIG"
   plaintext_value = digitalocean_kubernetes_cluster.primary.kube_config[0].raw_config
